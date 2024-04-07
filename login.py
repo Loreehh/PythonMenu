7ebb754ae471b97ade5460e7cd68de7f3095b1bb
Loreeh_ <128195197+Loreehh@users.noreply.github.com>
2024-04-07 19:05:31 +0200

    Added Colors and question

--- login.py
+++ login.py
@@ -1,12 +1,19 @@
-print("Inster your username")
-username = input()
+print('\033[92mInsert your username')
+username = input('\033[94m')
 
-print("Insert your password")
+print('\033[92mInsert your password')
 password = input()
 if username == "admin":
 	if password == "1234":
-		print("Access Granted")
+		print("\033[92mAccess Granted")
 	elif password != "1234":
-		print("Access Denied")
+		print("\033[93mAccess Denied")
 else:
-	print("Access Denied")
+	print("\033[93mAccess Denied")
+
+
+print("\033[92mWhat do you want to do?")
+choose = input()
+
+if choose == "close":
+	print("\033[93mClosing Menu")
