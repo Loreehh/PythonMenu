c9268ab7d8274d89c6de6e0498fb85b2a33fa797
Loreeh_ <128195197+Loreehh@users.noreply.github.com>
2024-04-18 17:28:29 +0200

    BUG FIX + ADDON
    Fixed a login.py bug when ascii font was sending errors in console
    Fixed in login.py & app.py the color
    Added a temporanly "Press the enter key for exit! :D"
    Added options list on app.py & login.py

--- login.py
+++ login.py
@@ -8,18 +8,25 @@ password = input()
 if username == "admin":
 
 	if password == "1234":
-		print("\033[92mAccess Granted\033[0m")
+		print("\033[92mAccess Granted")
 		x = open("test.txt", "w+")
 		x.write(f"Username: {username}\nPassword: {password}")
+
 	elif password != "1234":
 		print("\033[93mAccess Denied\033[0m")
+
 else:
 	print("\033[93mAccess Denied\033[0m")
 
 
 print("\033[92mWhat do you want to do?\033[0m")
 
-choose = input()
+print("""
+\x1b[38;2;0;255;58m[1] Close
+[2] Menu
+	  """)
+
+choose = input("\x1b[38;2;0;255;58m>>> \x1b[0;0;0;0;00m")
 
 if choose == "close":
 	print("\033[93mClosing Menu\033[0m")
