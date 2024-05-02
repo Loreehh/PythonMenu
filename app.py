6b4b87e2df6977e80a8912343d75defc37643f6e
Loreeh_ <128195197+Loreehh@users.noreply.github.com>
2024-05-02 18:36:38 +0200

    Added a few features
    Now all the features will be open only by  a number
    Insert functions 4,5,6,8
    Added Text Animation to function 8
    Rewrited Function 1

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 import webbrowser
+import os
+from time import sleep
+import sys
+
+def typewriter_effect(text):
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        sleep(0.15)  
+    print()
 
 print("""
  █████  ██████  ██████      ███    ███ ███████ ███    ██ ██    ██ 
@@ -11,18 +21,15 @@ print("\033[92mMade by Loreeh_\033[0m")
 print("""
       
       
-      
-      
-      
       """)
 
 
 print("\x1b[0;0;0;0;00mWhat do you want to do?")
 
 print("""
-\x1b[38;2;0;255;58m[1] Close
-[2] Credentials
-[3] Youtube
+\x1b[38;2;0;255;58m[1] GitHub                           [4] Upcase
+[2] Credentials                      [5] Capitalize                      [8] Open CMD
+[3] Youtube                          [6] Lower                           [9] 
 
 """) # Here you can add more options
 
@@ -31,12 +38,13 @@ choose = input("\x1b[38;2;0;255;58m>>> \x1b[0;0;0;0;00m")
 
 # Here you can add or modify the options
 
-if choose == "close" :
-    print("Program closed.")
+if choose == "1" :
+    print("Opening my Github Profile.")
+    webbrowser.open('https://github.com/Loreehh')
     
 
 f = open('test.txt','r')
-if choose == "credentials" :
+if choose == "2" :
     print(" ")
     with open('test.txt') as f:
         contents = f.read()
@@ -44,8 +52,28 @@ if choose == "credentials" :
     print(" ")
     input("\x1b[38;2;0;255;58mPress the enter key for exit! :D\x1b[0;0;0;0;00m")
 
-if choose == "youtube":
+if choose == "3":
     webbrowser.open('http://youtube.com')
     print(" ")
     input("\x1b[38;2;0;255;58mPress the enter key for exit! :D\x1b[0;0;0;0;00m")
+
+if choose == "4":
+    print("Enter what do you want")
+    choose = input("\x1b[38;2;0;255;58m>>> \x1b[0;0;0;0;00m")
+    print(choose.upper())
+
+if choose == "5":
+    print("Enter what do you want")
+    choose = input("\x1b[38;2;0;255;58m>>> \x1b[0;0;0;0;00m")
+    print(choose.capitalize())
+
+if  choose == "6":
+    print("Enter what do you want")
+    choose = input("\x1b[38;2;0;255;58m>>> \x1b[0;0;0;0;00m")
+    print(choose.lower())
+
+if choose == "8":
+    line_1 = "Opening Command Prompt ....."
+    typewriter_effect(line_1)
+    os.system("start cmd")
 
